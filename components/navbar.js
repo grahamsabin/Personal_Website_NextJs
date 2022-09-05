1d909f8c72dfa10fc2f58b5b95eda7f41a1cd92a
Graham Sabin <grahamcsabin@outlook.com>
2022-09-05 12:47:27 -0700

    links working, mobile friendly

--- components/navbar.js
+++ components/navbar.js
@@ -5,16 +5,55 @@ export default function Navbar() {
     return (
         <div className={`${styles.padding}`}>
             <nav className={`navbar-menu ${styles.border}`} role="navigation" aria-label="main navigation">
-                
 
-                <div id="navbarBasicExample" className={`navbar-menu`}>
-                    <div className={`navbar-brand`}>
-                        <a className={`navbar-item ${styles.nameClick}`}>
-                            Graham Sabin
+                <div className={`navbar-brand`}>
+                    <a className={`navbar-item ${styles.nameClick}`} href="/">
+                        Graham Sabin
+                    </a>
+                </div>
+
+                <div className={`navbar-menu`}>
+                    <div className={`navbar-start ${styles.rightNavLayout}`}>
+                        <a className={`navbar-item ${styles.leftNavContent}`}>
+                            DEVELOPER, ENTREPRENEUR
                         </a>
                     </div>
 
-                    <div className={`navbar-start ${styles.rightNavLayout}`}>
+                    <div className={`navbar-end ${styles.rightNavLayout}`}>
+                        <a className={`navbar-item ${styles.rightNavContent}`} href="/work">
+                            work,
+                        </a>
+                        <a className={`navbar-item ${styles.rightNavContent}`} href="/about">
+                            about,
+                        </a>
+                        <a className={`navbar-item ${styles.rightNavContent}`} href="/contact">
+                            contact
+                        </a>
+                    </div>
+                </div>
+            </nav>
+
+            <nav className={`navbar-menu is-hidden-desktop is-active ${styles.border}`} role="navigation" aria-label="main navigation">
+                
+                    <div className={`navbar-brand`}>
+                        <a className={`navbar-item ${styles.nameClickMobile}`}>
+                            Graham Sabin
+                        </a>
+
+                        <a className={`navbar-item ${styles.rightNavContentMobile}`}>
+                            work,
+                        </a>
+
+                        <a className={`navbar-item ${styles.rightNavContentMobile}`}>
+                            about,
+                        </a>
+
+                        <a className={`navbar-item ${styles.rightNavContentMobile}`}>
+                            contact
+                        </a>
+                    </div>
+                <div className={`navbar-menu`}>
+                    {/* <div className={`navbar-start ${styles.rightNavLayout}`}>
                         <a className={`navbar-item ${styles.rightNavContent}`}>
                             DEVELOPER, ENTREPRENEUR
                         </a>
@@ -30,7 +69,7 @@ export default function Navbar() {
                         <a className={`navbar-item ${styles.rightNavContent}`}>
                             contact
                         </a>
-                    </div>
+                    </div> */}
                 </div>
             </nav>
         </div>
--- pages/landingPage.js
+++ pages/landingPage.js
@@ -4,9 +4,9 @@ import style from '../scss/landingPage.module.scss';
 export default function LandingPage() {
     return(
         <>
-            <div className={`columns is-multiline is-desktop`}>
+            <div className={`columns is-multiline`}>
 
-                <div className={`column is-7 ${style.mainPadding}`}>
+                <div className={`column is-7 is-hidden-mobile ${style.mainPadding}`}>
                     <div className={`columns is-multiline`}>
                         <div className={`column is-full ${style.helloText}`}>
                             <p>Hello.</p>
@@ -27,8 +27,34 @@ export default function LandingPage() {
                     </div>
                 </div>
 
+                <div className={`column is-7 is-hidden-tablet ${style.mainPadding}`}>
+                    <div className={`columns is-multiline`}>
+                        <div className={`column is-full ${style.helloTextMobile}`}>
+                            <p>Hello.</p>
+                        </div>
+                        <div className={`column is-full ${style.iAmTextMobile}`}>
+                            <p>I am Graham</p>
+                        </div>
+                        {/* <div className={`column is-full ${style.grahamMobile}`}>
+                            <p>Graham</p>
+                        </div> */}
+                        <div className={`column is-full is-full-mobile ${style.descriptionTextMobile}`}>
+                            <p>
+                                What&apos;s up! I&apos;m a developer and student out of Seattle, Washington. I&apos;m into working
+                                on wild products and I&apos;ve spent my short career surrounded by them. I&apos;ve built a 
+                                startup (unsuccesful) and worked at a pretty cool one (still succesful), so I&apos;ve gained
+                                some great experience bringing products to life and making things happen. Looking for 
+                                what&apos;s next!
 
-                <div className={`column is-5 ${style.imageMargin}`}>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                
+
+
+                <div className={`column is-5-desktop ${style.imageMargin}`}>
                         {/* <figure className={`image is-2by3 ${style.imageMargin}`}> */}
                             <img src="person_image.jpg" className={`${style.imageDiv}`}/>
                         {/* </figure>   */}
