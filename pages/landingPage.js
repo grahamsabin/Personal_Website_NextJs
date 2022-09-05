a0ac2b0538926c2baaf68035088c4a3955cc4aac
Graham Sabin <grahamcsabin@outlook.com>
2022-09-04 22:37:52 -0700

    update issues

--- pages/landingPage.js
+++ pages/landingPage.js
@@ -16,11 +16,11 @@ export default function LandingPage() {
                         </div>
                         <div className={`column is-full is-full-mobile ${style.descriptionText}`}>
                             <p>
-                                What's up! I'm a developer and student out of Seattle, Washington. I'm into working
-                                on wild products and I've spent my short career surrounded by them. I've built a 
-                                startup (unsuccesful) and worked at a pretty cool one (still succesful), so I've gained
+                                What&apos;s up! I&apos;m a developer and student out of Seattle, Washington. I&apos;m into working
+                                on wild products and I&apos;ve spent my short career surrounded by them. I&apos;ve built a 
+                                startup (unsuccesful) and worked at a pretty cool one (still succesful), so I&apos;ve gained
                                 some great experience bringing products to life and making things happen. Looking for 
-                                what's next!
+                                what&apos;s next!
 
                             </p>
                         </div>
@@ -30,7 +30,7 @@ export default function LandingPage() {
 
                 <div className={`column is-5 ${style.imageMargin}`}>
                         {/* <figure className={`image is-2by3 ${style.imageMargin}`}> */}
-                            <img src="person_image.jpg" className={`${style.imageDiv}`}/>
+                            <Image src="person_image.jpg" className={`${style.imageDiv}`}/>
                         {/* </figure>   */}
                 </div>
                 
