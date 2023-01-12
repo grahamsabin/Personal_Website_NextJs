c0669363d86e8128be7497111e073439c8d87951
Graham Sabin <grahamcsabin@outlook.com>
2023-01-11 18:09:40 -0800

    day 4 - jokes page done except some styling for it

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import LandingPage from "./landingPage"
 import Projects from "./projects"
-import MyApp from "./_app"
 
 export default function Home() {
   return (
--- pages/jokes.js
+++ pages/jokes.js
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 import style from '../scss/jokes.module.scss';
+import jokes from '../data/jokes.json';
 
 export default function Jokes() {
     
+    
+
     const [firstClick, setFirstClick] = useState(true);
     const [showJoke, setShowJoke] = useState(false);
     const [showPunch, setShowPunch] = useState(false);
+    const [randomNum, setRandomNum] = useState(0);
     
     const  ToggleJokes = () => {
         setFirstClick(false);
+        setShowPunch(false);
+        setRandomNum(Math.floor(Math.random() * (Object.keys(jokes.jokes).length)));
         setShowJoke(true);
+          
     }
 
     const  ShowPunch = () => {
@@ -18,27 +25,42 @@ export default function Jokes() {
 
     return (
         <>
-            <div className={`${style.introTextSpacing} ${style.center}`}>
+            <div className={`${style.introTextSpacing} `}>
                 <h1 className={`${style.introText}`}>
                     People don't laugh enough. Take yourself less seriously, get ready to chuckle, and click the button for a random joke. 
                 </h1>
             </div>
 
-            <div>
-                <button onClick={ToggleJokes}>{firstClick ? "the button" : "again"}</button>
+            <div className={`columns is-multiline ${style.center}`}>
+                
+                
+                <div className={``}>
+                    <button className={`button ${style.jokeButton}`} onClick={ToggleJokes}>{firstClick ? "the button" : "again"}</button>
+                </div>
+                
                 
                 {showJoke ? 
-                    <div>
-                        <p>What happened when the chicken crossed the rode?</p>
-                        <button onClick={ShowPunch}>{showPunch ? "Hide Punchline" : "Show Punchline"}</button>
-                    </div> 
+                   <>
+                        <div className={``}>
+                            <p>{jokes.jokes[randomNum].joke}</p>
+                        </div>
+                        {/* <div className={``}>    
+                            <button className={`button ${style.punchButton}`} onClick={ShowPunch}>{showPunch ? "Hide Punchline" : "Show Punchline"}</button>
+                        </div>  */}
+
+                        <div className={`box ${style.punchlineDoor}`}>
+                            <p1 className={`${style.p1}`}>hover...</p1>
+                            <p className={`${style.punchText}`}>{jokes.jokes[randomNum].punchline}</p>
+                            {/* {showPunch ?
+                            <p>{jokes.jokes[randomNum].punchline}</p>
+                            : <></>
+                            }  */}
+                        </div>
+                    </>
                     : <></>
                 }
 
-                {showPunch ?
-                    <p>It never even crossed!</p>
-                    : <></>
-                }
+                
             </div>
             
 
